7d2c57ace6a39a0c0d167b71aa4cbf84b810ece3
alejandro <alejandro.w.guerra@gmail.com>
2023-12-27 19:54:20 -0300

    Update production.rb
    add hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << "rails-production-97ca.up.railway.app"
 end
